f8ac35c964ec5e6669131f28e53d82b02c805f2a
Udit2008 <Pathakudit2008@gmail.com>
2022-11-19 15:20:17 +0530

    AnimeKudasai!

---
+++ assets/JS/Main.js
@@ -0,0 +1,50 @@
+const header = document.querySelector("header");
+const prt_section = document.querySelector(".portfolio");
+const zoom_icon = document.querySelectorAll(".zoom-icon");
+const modal_overlay = document.querySelector(".modal-overlay");
+const images = document.querySelectorAll(".images img");
+
+function stickyNavbar(){
+    header.classList.toggle("scrolled", window.pageYOffset > 0);
+}
+
+stickyNavbar();
+window.addEventListener("scroll", stickyNavbar);
+
+let mixer = mixitup(".portfolio-gallery");
+
+let currentIndex = 0;
+zoom_icon.forEach((icn, i) => 
+    icn.addEventListener("click", () => {
+    prt_section.classList.add("open");
+    document.body.classList.add("stopScrolling");
+    currentIndex = i;
+    changeImage(currentIndex);
+    })
+);
+
+modal_overlay.addEventListener("click", () => {
+    prt_section.classList.remove("open");
+    document.body.classList.remove("stopScrolling");
+});
+
+function changeImage(index) {
+    images.forEach((img) => img.classList.remove("showImage"));
+    images[index].classList.add("showImage");
+}
+document.addEventListener('contextmenu', (e) => e.preventDefault());
+
+function ctrlShiftKey(e, keyCode) {
+  return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+}
+
+document.onkeydown = (e) => {
+  if (
+    event.keyCode === 123 ||
+    ctrlShiftKey(e, 'I') ||
+    ctrlShiftKey(e, 'J') ||
+    ctrlShiftKey(e, 'C') ||
+    (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
+  )
+    return false;
+};
